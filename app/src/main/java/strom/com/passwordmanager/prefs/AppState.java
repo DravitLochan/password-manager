856d34d35a0948b66990557dda212a7662c1b3c8
Cahyo Fendhi <cahyofendhi@gmail.com>
2017-10-08 02:18:25 +0700

    open setPin activity in the first time (#5)
    * open setPin activity in the first time
    
    * setPin
    
    check pin
    
    * Update AndroidManifest.xml
    
    * Update AppState.java
    
    * check finger print
    
    * Update misc.xml
    
    * Update modules.xml

--- app/src/main/java/strom/com/passwordmanager/prefs/AppState.java
+++ app/src/main/java/strom/com/passwordmanager/prefs/AppState.java
@@ -2,6 +2,7 @@ package strom.com.passwordmanager.prefs;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 /**
  * Created by DravitLochan on 23-09-2017.
@@ -15,6 +16,8 @@ public class AppState {
 
     String PREF_NAME = "password-checkfetchdetails";
     String STATE = "state";
+    String IS_FIRST = "CHECK_OPEN_FIRST";
+    String IS_VERIFIED = "CHECK_IS_VERIFIED";
 
     public AppState(Context context) {
         this.context = context;
@@ -22,12 +25,36 @@ public class AppState {
         editor = sharedPreferences.edit();
     }
 
+    public void setIsVerified(){
+        Log.d("status_verified", "check");
+        editor.putBoolean(IS_VERIFIED, true);
+        editor.commit();
+    }
+
     public boolean isVerified() {
-        return sharedPreferences.getBoolean(STATE, false);
+        return sharedPreferences.getBoolean(IS_VERIFIED, false);
     }
 
     public void setVerified(boolean state) {
         editor.putBoolean(STATE, state);
         editor.commit();
     }
+
+    //start
+    /**
+     * set when have open in first time
+     */
+    public void setHaveOpened(){
+        editor.putBoolean(IS_FIRST, true);
+        editor.commit();
+    }
+
+    /**
+     * get status app opened
+     * @return
+     */
+    public boolean isOpened(){
+        return sharedPreferences.getBoolean(IS_FIRST, false);
+    }
+    // end
 }
